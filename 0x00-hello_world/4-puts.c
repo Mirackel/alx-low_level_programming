9df4aa6fbe7807a7ff7c27725ec0f940da62a365
miracle gbaghara <gbagharamiracle4@gmail.com>
2023-02-19 10:19:57 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - entry poit
- *return: always 0 (success)
+ * main - main block
+ *return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle*);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

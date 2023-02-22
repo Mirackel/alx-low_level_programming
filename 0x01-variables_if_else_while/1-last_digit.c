d2961f6ea35165b74e51067cbd4e6fcdb7c7ac53
miracle gbaghara <gbagharamiracle4@gmail.com>
2023-02-21 21:41:55 -0800

    variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,11 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - prints the last digit of a randomly generated number
+ * and whether it is greater than 5, less than 6, or 0.
+ *
  * Return: Always 0
  */
 int main(void)
@@ -26,5 +28,6 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is  0\n", n, n % 10);
 	}
+	putchar('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints all number in base 16 lowercase
- * Return: Always 0
+ * main - prints numbers between 0 to 9 and letters between a to f.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -11,13 +12,11 @@ int main(void)
 	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
-		}
-
-	for (i = 97; i  <= 103; i++)
+	}
+	for (i = 97; i < 103; i++)
 	{
 		putchar(i);
 		}
 	putchar('\n');
 	return (0);
 }
-
